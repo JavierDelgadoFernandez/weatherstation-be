fcb7de5c0af97142dc8af10d14815ae468ef7915
Javier Delgado <JavierDelgado@outlook.com>
2015-10-03 19:18:49 +0200

    Added charts of the last day.

--- app/views.py
+++ app/views.py
@@ -2,7 +2,8 @@ from app import app, db
 from models import Measure
 from sqlalchemy import desc
 from flask import render_template, jsonify, request
-from datetime import datetime
+from datetime import datetime, timedelta
+
 
 def last_measure():
 	result = {}
@@ -13,16 +14,35 @@ def last_measure():
 	result['pressure'] = measure.pressure
 	return result
 
+
 @app.route('/', methods=['GET'])
 @app.route('/home', methods=['GET'])
 def home():
 	measure = last_measure()
 	return render_template('home.html', measure=measure)
 
+
+@app.route('/last_day', methods=['GET'])
+def chart():
+	yesterday = datetime.now() - timedelta(days=1)
+	measures = Measure.query.filter(Measure.date > yesterday).all()
+	if (len(measures) > 0):
+		temperatures = map(lambda measure : [ datetime_epoch(measure.date), measure.temperature ], measures)
+		pressures = map(lambda measure : [ datetime_epoch(measure.date), measure.pressure ], measures)
+		humidities = map(lambda measure : [ datetime_epoch(measure.date), measure.humidity ], measures)
+		return render_template('chart.html', temperatures=temperatures, pressures=pressures, humidities=humidities)
+	return render_template('chart.html')
+
+@app.route('/about', methods=['GET'])
+def about():
+	return render_template('about.html')
+
+
 @app.route('/api/measure/', methods=['GET'])
 def api_last_measure():
 	measure = last_measure()
 	return jsonify(measure)
+
 
 @app.route('/api/add_measure/', methods=['POST'])
 @app.route('/api/measure/', methods=['POST'])
@@ -36,3 +56,8 @@ def add_measure():
 	db.session.commit()
 	return jsonify({'result': True}), 201
 
+
+def datetime_epoch(dt):
+	epoch = datetime.utcfromtimestamp(0)
+	delta = dt - epoch
+	return ((delta.microseconds + (delta.seconds + delta.days * 24 * 3600) * 10**6) / 10**6) * 1000
